763efaeaf85bf3daa57e0e2a6cda2e85540d084b
rickydeluca <rickydeluca1@gmail.com>
2020-05-19 16:53:58 +0200

    chang program logic

--- color_retrieval.cpp
+++ color_retrieval.cpp
@@ -7,112 +7,90 @@
 using namespace cv;
 using namespace std;
 
-Mat compareColorSpace(Mat img, String img_database_path) {
-	// convert rgb to hsv
-	Mat hsv_img;
-	cvtColor(img, hsv_img, cv.COLOR_BGR2HSV);
-
-	// quantify hsv histogram
-	Mat hsv_planes[3];
-	split(hsv_img, hsv_planes); // separete the h, s, v channels
-	Mat h_plane = hsv_planes[0];
-	Mat s_plane = hsv_planes[1];
-	Mat v_plane = hsv_planes[2];
-
-	Mat hsv_quantified[3];
-	int x, y;
-	for (x = 0; x < hsv_img.height; x++) {
-		for (y = 0; y < hsv_img.width; y++) {
-			Vec3f h_value = h_plane.at<Vec3f>(y, x);
-			
-			if (h_value >= 316 && h_value <= 360) {
-				hsv_quantified[0].at<Vec3f>(y, x) = 0;
-			}
-
-			else if (h_value >= 1 && h_value <= 25) {
-				hsv_quantified[0].at<Vec3f>(y, x) = 1;
-			}
-
-			else if (h_value >= 26 && h_value <= 40) {
-				hsv_quantified[0].at<Vec3f>(y, x) = 2;
-			}
-
-			else if (h_value >= 41 && h_value <= 120) {
-				hsv_quantified[0].at<Vec3f>(y, x) = 3;
-			}
-
-			else if (h_value >= 121 && h_value <= 190) {
-				hsv_quantified[0].at<Vec3f>(y, x) = 4;
-			}
-
-			else if (h_value >= 191 && h_value <= 270) {
-				hsv_quantified[0].at<Vec3f>(y, x) = 5;
-			}
-
-			else if (h_value >= 271 && h_value <= 295) {
-				hsv_quantified[0].at<Vec3f>(y, x) = 6;
-			}
-
-			else if (h_value >= 296 && h_value <= 315) {
-				hsv_quantified[0].at<Vec3f>(y, x) = 7;
-			}
-
-			Vec3f s_value = s_plane.at<Vec3f>(y, x);
-
-			if (s_value >= 0 && s_value < 0.2) {
-				hsv_quantified[1].at<Vec3f>(y, x) = 0;
-			}
-
-			else if (s_value >= 0.2 && s_value < 0.7) {
-				hsv_quantified[1].at<Vec3f>(y, x) = 2;
-			}
-
-			else if (s_value >= 0.7 && s_value <= 1) {
-				hsv_quantified[1].at<Vec3f>(y, x) = 3;
-			}
-
-			Vec3f v_value = v_plane.at<Vec3f>(y, x);
-
-			if (v_value >= 0 && v_value < 0.2) {
-				hsv_quantified[2].at<Vec3f>(y, x) = 0;
-			}
-
-			else if (v_value >= 0.2 && v_value < 0.7) {
-				hsv_quantified[2].at<Vec3f>(y, x) = 2;
-			}
-
-			else if (v_value >= 0.7 && v_value <= 1) {
-				hsv_quantified[2].at<Vec3f>(y, x) = 3;
-			}
-
-		} 
-	}
-
-	// scan the database convert each image 
-}
-
-
-int main (int argc, char** argv) {
-	// read an image and display the red channel
-	String img_database_path =  "image_database/";
-	Mat img = imread("image_database/img_017.JPG", IMREAD_COLOR);
-
-	// resize image
-	Size size(800, 600);
-	resize(img, img, size);
-
-	Mat planes[3];
-	split(img, planes);
-
+int compareColorSpace(Mat img, String img_database_path) {
 	// convert rgb to hsv
 	Mat hsv_img;
 	cvtColor(img, hsv_img, COLOR_BGR2HSV);
 
+	// split channels of the image
+	vector<Mat> channels;
+	split(hsv_img, channels);
+	Mat h0 = channels[0];
+	Mat s0 = channels[1];
+	Mat v0 = channels[2];
+
+	/*
+	// quantify hsv histogram using inRange
+	Mat img_threshold;
+
+	// first set
+	int low_h  = 157;
+	int high_h = 179;
+
+	int low_s  = 0;
+	int high_s = 51;
+
+	int low_v  = 0;
+	int high_v = 51;
+
+	inRange(hsv_img, Scalar(low_h, low_s, low_v), Scalar(high_h, high_s, high_v), img_threshold);
+
+	return img_threshold;
+	*/
+
+	// read the images from database
+	VideoCapture cap("/home/rickydeluca/Scrivania/università/tesi/image_retrieval/image_database/img_%3d.JPG"); // %3d means 001.JPG notation
+	if (!cap.isOpened()) // check if succeeded
+		return -1;
+
+	Size size(800, 600);
+	vector<Mat> database_channels;
+	Mat h1, s1, v1;		// channels for database_img
+	int dh, ds, dv; 	// distances from color spaces
+	float gcs_distance;	// global color space distance
+	for (;;) {
+		Mat database_img;
+		cap >> database_img;
+		resize(database_img, database_img, size);				// resize the image
+		cvtColor(database_img, database_img, COLOR_BGR2HSV);	// convert to hsv colorspace
+
+		// split channels of the database_img
+		split(database_img, database_channels);
+		h1 = database_channels[0];
+		s1 = database_channels[1];
+		v1 = database_channels[2];
+
+		// compute euclidian distance between img and database_img
+		dh = min(abs(h1-h0), 179 - abs(h1-h0));
+		ds = abs(s1-s0);
+		dv = abs(v1-v0);
+
+		gcs_distance = sqrt(dh*dh + ds*ds + dv*dv);
+
+		// insert found distance in distance_array
+	}
+
+	// TODO
+}
+
+
+int main (int argc, char** argv) {
+	String img_database_path =  "image_database/";
+	Mat img = imread("image_database/img_016.JPG", IMREAD_COLOR); // happy kiki
+
+	// resize the image
+	Size size(800, 600);
+	resize (img, img, size);
+
+	printf("Calling compareColorSpace\n\n");
+	Mat ret = compareColorSpace(img, img_database_path);
+
 	// dispalay images
     namedWindow("Display Image", WINDOW_AUTOSIZE);
-    imshow("Display window", planes[2]);
+    imshow("Display window", ret);
 
     // wait for a keystroke in the window
     int k = waitKey(0);
     return 0;
+
 }
